b9d1297123e3f9f4875d0f963bbb3de413dbaf6a
brunovgois <brunovgois@gmail.com>
2023-01-24 21:53:29 -0300

    html and css wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
+import "./App.css";
+import Logo from "./assets/Logo.svg";
+import plus from "./assets/plus.png";
+import Clipboard from "./assets/Clipboard.png";
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <header>
+        <img src={Logo} alt="Logo for the ToDo App" />
+      </header>
 
-    </div>
-  )
+      <div className="inputContainer">
+        <input type="text" />
+        <button>
+          <p>Criar</p>
+          <img src={plus} alt="" />
+        </button>
+      </div>
+
+      <div>
+        <div>
+          <p>Tarefas criadas</p>
+          <p>0</p>
+        </div>
+        <div>
+          <p>Concluídas</p>
+          <p>0</p>
+        </div>
+      </div>
+
+      <img src={Clipboard} alt="" />
+      <p>Você ainda não tem tarefas cadastradas</p>
+      <p>Cria tarefas e organize seus itens a fazer</p>
+    </>
+  );
 }
 
-export default App
+export default App;
